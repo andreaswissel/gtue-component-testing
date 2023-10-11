e1f1a9a1eb7b9eea75a2d407e3bb7f6e4d06ce51
Andreas Wissel <business@andreaswissel.com>
2023-10-11 23:31:35 +0200

    first commit

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,20 +1,5 @@
 describe('My First Test', () => {
-  beforeEach(() => {
-    cy.visit('/');
-    cy.get("[data-cy='order-button']").find('button').as('orderButton');
-  });
-
-  it('Visits the app, fills out the order form and confirms that the user is being redirected after submit', () => {
-    cy.get('#firstname').type('Max');
-    cy.get('#lastname').type('Mustermann');
-    cy.get('#address-autocomplete').type('Rheingoldhalle');
-    cy.get('.pac-matched').first().click();
-    cy.get('@orderButton').click();
-
-    // check that loading state is present
-    cy.get('.is-loading').should('exist');
-
-    // check redirect
-    cy.url().should('be.equal', 'http://localhost:4200/success');
+  it('Does not do much!', () => {
+    expect(true).to.equal(true);
   });
 });
