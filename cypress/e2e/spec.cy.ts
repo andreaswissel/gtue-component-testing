113e380079df968c9a0a7e943bcad33ec5dac5cd
Andreas Wissel <business@andreaswissel.com>
2023-10-12 22:57:54 +0200

    test string replace in action

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,29 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('#address-autocomplete').type('a');
+    cy.get('.pac-matched').as('firstResult');
+    cy.get('#address-autocomplete').clear();
+    cy.get('[data-cy=order-button]').find('button').as('orderButton');
+  });
+
+  it('Visits the application', () => {
+    cy.get('app-order').should('contain', 'Complete your order');
+  });
+
+  it('fills out the order form', () => {
+    // Order form auswählen
+    cy.get('#firstname').type('Max');
+    cy.get('#lastname').type('Mustermann');
+    cy.get('#address-autocomplete').type('GTü Prüfstelle');
+
+    cy.get('@firstResult').first().click();
+
+    cy.get('@orderButton').click();
+
+    cy.get('.is-loading').should('exist');
+    cy.get('.is-loading').should('be.visible');
+
+    cy.url().should('be.equal', 'http://localhost:4200/success');
   });
 });
--- projects/lego/src/lib/autocomplete/autocomplete.component.cy.ts
+++ projects/lego/src/lib/autocomplete/autocomplete.component.cy.ts
@@ -1,7 +1,41 @@
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AutocompleteComponent } from '@lego/components';
+import { Default } from './autocomplete.stories';
+
 describe('AutocompleteComponent', () => {
   const selectedOption = `Steak sandwhich`;
   const bbqRibsRegEx = /BBQ ribs/i;
 
   // TODO: Text "steak sa" eintippen, Option selektieren, pruefen, dass setOption mit dem richtigen Wert aufgerufen wurde
+
+  beforeEach(() => {
+    cy.mount(AutocompleteComponent, {
+      imports: [NoopAnimationsModule],
+      componentProperties: {
+        ...Default.args,
+      },
+    }).then((mountedComponent) => {
+      cy.spy(mountedComponent.component.onOptionSet, 'emit').as('setOptionSpy');
+    });
+  });
+
+  it('should select an option', () => {
+    cy.get('input').type('steak sa');
+    cy.findByRole('option', { name: selectedOption }).should('exist');
+    cy.findByRole('option', { name: selectedOption }).click();
+    cy.get('input').should('have.value', 'Steak sandwhich');
+
+    cy.get('@setOptionSpy').should('have.been.calledOnceWith', selectedOption);
+  });
+
   // TODO: Text löschen, "bbq" eintippen, Option selektieren, pruefen, dass setOption mit dem richtigen Wert aufgerufen wurde
+  it('should clear the input and if bbq is selected should emit "BBQ ribs', () => {
+    cy.get('input').clear();
+    cy.get('input').realType('bbq,');
+
+    cy.findByRole('option', { name: bbqRibsRegEx }).should('exist');
+    cy.findByRole('option', { name: bbqRibsRegEx }).click();
+
+    cy.get('@setOptionSpy').should('have.been.calledOnceWith', 'BBQ ribs');
+  });
 });
